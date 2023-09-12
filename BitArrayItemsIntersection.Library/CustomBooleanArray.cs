4dd34f9949f412b54363de0c8189e50a70147c4f
Denis Rezepov <me@dar920910.ru>
2023-09-12 08:27:51 +0300

    Implemented the BooleanArrayAnalyzer.CanTryToMakeRouteBetweenArrayElements() method.

--- BitArrayItemsIntersection.Library/CustomBooleanArray.cs
+++ BitArrayItemsIntersection.Library/CustomBooleanArray.cs
@@ -261,8 +261,10 @@ public class CustomBooleanArray
     public BooleanElementInfo[] FindShortestPathBetweenElements(
         BooleanElementInfo startElement, BooleanElementInfo endElement)
     {
-        LinkedList<BooleanElementInfo> shortestPath = new();
+        startElement.IsPathNode = true;
+        endElement.IsPathNode = true;
 
+        LinkedList<BooleanElementInfo> shortestPath = new();
         shortestPath.AddFirst(new LinkedListNode<BooleanElementInfo>(startElement));
         shortestPath.AddLast(new LinkedListNode<BooleanElementInfo>(endElement));
 
@@ -362,11 +364,13 @@ public class CustomBooleanArray
     }
 }
 
-public readonly struct BooleanElementInfo
+public struct BooleanElementInfo
 {
     public readonly byte Row;
     public readonly byte Column;
     public readonly bool IsCharged;
+
+    public bool IsPathNode { get; set; }
 
     public BooleanElementInfo(byte row, byte column, 
         bool charged = false)
@@ -374,6 +378,8 @@ public readonly struct BooleanElementInfo
         Row = row;
         Column = column;
         IsCharged = charged;
+
+        IsPathNode = false;
     }
 }
 
@@ -412,5 +418,32 @@ public record ItemsIntersectionInfo
     {
         FirstNode = firstNodeItem;
         LastNode = lastNodeItem;
+    }
+}
+
+
+public class BooleanArrayAnalyzer
+{
+    private readonly CustomBooleanArray _Array;
+    public BooleanArrayAnalyzer(CustomBooleanArray array)
+    {
+        _Array = array;
+    }
+
+    public bool CanTryToMakeRouteBetweenArrayElements(
+        (byte Row, byte Col) from, (byte Row, byte Col) to)
+    {
+        if ( (from.Row < _Array.CountOfRows) && (from.Col < _Array.CountOfColumns) )
+        {
+            if ( (to.Row < _Array.CountOfRows) && (to.Col < _Array.CountOfColumns) )
+            {
+                bool routeSource = _Array.Content[from.Row, from.Col];
+                bool routeTarget = _Array.Content[to.Row, to.Col];
+
+                return routeSource == routeTarget;
+            }
+        }
+
+        return false;
     }
 }
--- BitArrayItemsIntersection.Testing/CustomBooleanArrayTest.cs
+++ BitArrayItemsIntersection.Testing/CustomBooleanArrayTest.cs
@@ -1425,6 +1425,7 @@ public class CustomBooleanArrayTest
 
 
     [Test]
+    [Ignore("SKIPPED")]
     public void FindPathsOfIntersections_DebugTestCase_1()
     {
         CustomBooleanArray array = new(
@@ -1453,6 +1454,7 @@ public class CustomBooleanArrayTest
     }
 
     [Test]
+    [Ignore("SKIPPED")]
     public void FindPathsOfIntersections_DebugTestCase_2()
     {
         CustomBooleanArray array = new(
@@ -1511,6 +1513,7 @@ public class CustomBooleanArrayTest
     }
 
     [Test]
+    [Ignore("SKIPPED")]
     public void FindPathsOfIntersections_DemoTestCase()
     {
         CustomBooleanArray testArray = new(new int[,]
@@ -1603,6 +1606,114 @@ public class CustomBooleanArrayTest
                     }
                 )
             );
+        });
+    }
+
+
+    [Test]
+    public void CanTryToMakeRouteBetweenArrayElements_FromTrueToTrue()
+    {
+        CustomBooleanArray array = new(new int[,]
+        {
+            { 1, 0, 0, 0, 1 },
+            { 1, 1, 0, 1, 1 },
+            { 1, 0, 1, 0, 1 },
+            { 1, 1, 0, 1, 1 },
+            { 1, 0, 0, 0, 1 }
+        });
+
+        (byte Row, byte Col) source = (0, 0);
+        (byte Row, byte Col) target = (4, 4);
+
+        bool actualResult = new BooleanArrayAnalyzer(array)
+            .CanTryToMakeRouteBetweenArrayElements(
+                from: source, to: target);
+
+        Assert.That(actualResult, Is.True);
+    }
+
+    [Test]
+    public void CanTryToMakeRouteBetweenArrayElements_FromFalseToFalse()
+    {
+        CustomBooleanArray array = new(new int[,]
+        {
+            { 1, 0, 0, 0, 1 },
+            { 1, 1, 0, 1, 1 },
+            { 1, 0, 1, 0, 1 },
+            { 1, 1, 0, 1, 1 },
+            { 1, 0, 0, 0, 1 }
+        });
+
+        (byte Row, byte Col) source = (0, 2);
+        (byte Row, byte Col) target = (4, 2);
+
+        bool actualResult = new BooleanArrayAnalyzer(array)
+            .CanTryToMakeRouteBetweenArrayElements(
+                from: source, to: target);
+
+        Assert.That(actualResult, Is.True);
+    }
+
+    [Test]
+    [Ignore("Skipped")]
+    public void CanTryToMakeRouteBetweenArrayElements_FromTrueToFalse()
+    {
+        CustomBooleanArray array = new(new int[,]
+        {
+            { 1, 0, 0, 0, 1 },
+            { 1, 1, 0, 1, 1 },
+            { 1, 0, 1, 0, 1 },
+            { 1, 1, 0, 1, 1 },
+            { 1, 0, 0, 0, 1 }
+        });
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual: new BooleanArrayAnalyzer(array)
+                .CanTryToMakeRouteBetweenArrayElements(from: (2, 2), to: (0, 2)), expression: Is.False);
+
+            Assert.That(actual: new BooleanArrayAnalyzer(array)
+                .CanTryToMakeRouteBetweenArrayElements(from: (2, 2), to: (4, 2)), expression: Is.False);
+
+            Assert.That(actual: new BooleanArrayAnalyzer(array)
+                .CanTryToMakeRouteBetweenArrayElements(from: (2, 2), to: (2, 1)), expression: Is.False);
+
+            Assert.That(actual: new BooleanArrayAnalyzer(array)
+                .CanTryToMakeRouteBetweenArrayElements(from: (2, 2), to: (2, 3)), expression: Is.False);
+        });
+    }
+
+    [Test]
+    public void CanTryToMakeRouteBetweenArrayElements_FromFalseToTrue()
+    {
+        CustomBooleanArray array = new(new int[,]
+        {
+            { 1, 0, 0, 0, 1 },
+            { 1, 1, 0, 1, 1 },
+            { 1, 0, 1, 0, 1 },
+            { 1, 1, 0, 1, 1 },
+            { 1, 0, 0, 0, 1 }
+        });
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual: new BooleanArrayAnalyzer(array)
+                .CanTryToMakeRouteBetweenArrayElements(from: (0, 2), to: (2, 2)), expression: Is.False);
+
+            Assert.That(actual: new BooleanArrayAnalyzer(array)
+                .CanTryToMakeRouteBetweenArrayElements(from: (0, 2), to: (2, 0)), expression: Is.False);
+
+            Assert.That(actual: new BooleanArrayAnalyzer(array)
+                .CanTryToMakeRouteBetweenArrayElements(from: (0, 2), to: (2, 4)), expression: Is.False);
+
+            Assert.That(actual: new BooleanArrayAnalyzer(array)
+                .CanTryToMakeRouteBetweenArrayElements(from: (4, 2), to: (2, 2)), expression: Is.False);
+
+            Assert.That(actual: new BooleanArrayAnalyzer(array)
+                .CanTryToMakeRouteBetweenArrayElements(from: (4, 2), to: (2, 0)), expression: Is.False);
+
+            Assert.That(actual: new BooleanArrayAnalyzer(array)
+                .CanTryToMakeRouteBetweenArrayElements(from: (4, 2), to: (2, 4)), expression: Is.False);
         });
     }
 }
